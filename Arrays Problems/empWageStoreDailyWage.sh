8d92b91b0c1ba9192ab2f853e4386399ed951ab9
pranav8596 <pranav.ige96@gmail.com>
2020-03-24 13:56:13 +0530

    ADDED: Program to store Daily wage of an Employee using Arrays

---
+++ Arrays Problems/empWageStoreDailyWage.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -x
+
+#Constants
+IS_PART_TIME=1
+IS_FULL_TIME=2
+EMP_RATE_PER_HOUR=20
+MAX_HRS_IN_MONTH=100
+MAX_WORKING_DAYS=20
+
+#Variables
+totalWorkHrs=0
+totalWorkDays=0
+
+#Declaration of an array
+declare -a employeeDailyWage
+
+#To get the number of Hours the employee works
+function getWorkingHours() {
+	ramdomCheck=$((RANDOM%3))
+	case $ramdomCheck in
+		$IS_PART_TIME)
+			empHours=4
+			;;
+		$IS_FULL_TIME)
+			empHours=8
+			;;
+		*)
+			empHours=0
+			;;
+	esac
+	echo $empHours
+}
+
+#To get Daily wage of the Employee 
+function getDailyWage() {
+	workHours=$1
+	wage=$(($workHours * $EMP_RATE_PER_HOUR))
+	echo $wage
+}
+
+#Calculating wages till working days or working hours reaches to a limit
+while [[ $totalWorkHrs -lt $MAX_HRS_IN_MONTH && $totalWorkDays -lt $MAX_WORKING_DAYS ]]
+do
+	((totalWorkDays++))
+	workHours="$(getWorkingHours)"
+	totalWorkHrs=$(($totalWorkHrs + $workHours))
+	employeeDailyWage[$totalWorkDays]="$(getDailyWage $workHours)"
+done
+
+totalSalery=$(( $EMP_RATE_PER_HOUR * $totalWorkHrs ))
+echo -e "Daily wage of a month:   ${employeeDailyWage[@]}"
+echo "Total salery of a month: $totalSalery"
